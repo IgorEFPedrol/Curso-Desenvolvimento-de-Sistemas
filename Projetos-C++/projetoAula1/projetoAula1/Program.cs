2fd69433193c41040d21df64f11b76cac02d8843
Igor Enrico <igorefp@gmail.com>
2022-02-10 19:31:15 -0300

    Update Program.cs

--- Projetos-C++/projetoAula1/projetoAula1/Program.cs
+++ Projetos-C++/projetoAula1/projetoAula1/Program.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace projetoAula01
+namespace projetoAula1
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            Console.BackgroundColor = ConsoleColor.Black;
         }
     }
 }
