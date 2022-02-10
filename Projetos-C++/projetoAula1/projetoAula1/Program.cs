8957756743b8a91a4de12396758b4df5578d22b1
Igor Enrico <igorefp@gmail.com>
2022-02-10 20:07:01 -0300

    Update projetoAula1

--- Projetos-C++/projetoAula1/projetoAula1/Program.cs
+++ Projetos-C++/projetoAula1/projetoAula1/Program.cs
@@ -10,10 +10,20 @@ namespace projetoAula1
     {
         static void Main(string[] args)
         {
+            setup();
+            Media();
+            Console.ReadKey();
+        }
+
+        public static void setup(){
             Console.BackgroundColor = ConsoleColor.Black;
-            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
             Console.Clear();
             Console.WriteLine("PROGRAMA DE CÁLCULO DA MÉDIA");
+        }
+
+        public static void Media()
+        { 
             double a = 0;
             double b = 0;
             Console.Write("Digite o valor de a: ");
@@ -21,7 +31,8 @@ namespace projetoAula1
             Console.Write("Digite o valor de b: ");
             b = Convert.ToDouble(Console.ReadLine());
             double media = (a + b) / 2;
-
+            Console.WriteLine("A média de {0:C2} + {1:C2} = {2:C2} ", a, b, media);
+            Console.ReadKey();
         }
     }
 }
