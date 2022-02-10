7301b1452701c901fdf980874e9a8f5375886145
Igor Enrico <igorefp@gmail.com>
2022-02-10 19:44:15 -0300

    Update Program.cs

--- Projetos-C++/projetoAula1/projetoAula1/Program.cs
+++ Projetos-C++/projetoAula1/projetoAula1/Program.cs
@@ -12,6 +12,15 @@ namespace projetoAula1
         {
             Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.WriteLine("PROGRAMA DE CÁLCULO DA MÉDIA");
+            double a = 0;
+            double b = 0;
+            Console.Write("Digite o valor de a: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o valor de b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+
         }
     }
 }
