88a388c59f79fcbc0101954065f72b6ca39579f8
Igor Enrico <igorefp@gmail.com>
2022-02-11 22:22:59 -0300

    Update projetoAula02

--- Projetos-C++/Aula02/projetoAula02/Program.cs
+++ Projetos-C++/Aula02/projetoAula02/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace projetoAula02
@@ -14,16 +15,17 @@ namespace projetoAula02
             Console.ForegroundColor = ConsoleColor.White;
             Console.Clear();
             Console.WriteLine("ÁREA DO CIRCULO");
-            CalculoArea();
+            for (int i = 1; i <= 50; i++)
+            {
+                CalculoArea(i);
+                Thread.Sleep(500);
+            }
             Console.ReadKey();
         }
-        public static void CalculoArea()
+        public static void CalculoArea(double raio)
         {
-            double raio;
-            Console.Write("\nDigite o raio: ");
-            raio = Convert.ToDouble(Console.ReadLine());
             double area = Math.PI * Math.Pow(raio, 2);
-            Console.WriteLine("A área do circulo é {0:N2}", area);
+            Console.WriteLine("A área do circulo de raio {0} é {1:N2}",raio , area);
         }                
     }
 }
