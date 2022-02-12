454bac8f467ecefce647d2115eace93c7ed31f0f
Igor Enrico <igorefp@gmail.com>
2022-02-11 21:50:26 -0300

    Update projetoAula02

--- Projetos-C++/Aula02/projetoAula02/Program.cs
+++ Projetos-C++/Aula02/projetoAula02/Program.cs
@@ -13,8 +13,13 @@ namespace projetoAula02
             Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.White;
             Console.Clear();
-            Console.WriteLine("ÁREA DO CIRCULO");
-
+            Console.WriteLine("AREA DO CIRCULO");
+            double raio;
+            Console.Write("\nDigite o raio: ");
+            raio = Convert.ToDouble(Console.ReadLine());
+            double area = Math.PI * Math.Pow(raio, 2);
+            Console.WriteLine("Area do circulo e {0:N2}", area);
+            Console.ReadKey();
         }
     }
 }
