e4cf8b0596a432ff0cebc69af11ad3a552c1f0b9
Igor Enrico <igorefp@gmail.com>
2022-02-15 21:38:41 -0300

    Update projetoMediaNumerica

--- Projetos-C#/MediaNumerica/projetoMediaNumerica/Form1.Designer.cs
+++ Projetos-C#/MediaNumerica/projetoMediaNumerica/Form1.Designer.cs
@@ -28,15 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(147, 214);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(134, 24);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(39, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Clique";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(291, 261);
+            this.ClientSize = new System.Drawing.Size(393, 351);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
+            this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CALCULADORA MÉDIA";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -45,6 +66,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
 
     }
 }
