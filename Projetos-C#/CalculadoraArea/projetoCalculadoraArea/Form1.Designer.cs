869c577857003cb9a453fc9f349b4b4dad0c2620
Igor Enrico <igorefp@gmail.com>
2022-02-17 19:28:27 -0300

    update projetoCalculadoraArea

--- Projetos-C#/CalculadoraArea/projetoCalculadoraArea/Form1.Designer.cs
+++ Projetos-C#/CalculadoraArea/projetoCalculadoraArea/Form1.Designer.cs
@@ -28,14 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRaio = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.Location = new System.Drawing.Point(133, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite o raio do Círculo:";
+            // 
+            // txtRaio
+            // 
+            this.txtRaio.Location = new System.Drawing.Point(137, 59);
+            this.txtRaio.Name = "txtRaio";
+            this.txtRaio.Size = new System.Drawing.Size(201, 20);
+            this.txtRaio.TabIndex = 1;
+            this.txtRaio.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
             this.ClientSize = new System.Drawing.Size(471, 378);
+            this.Controls.Add(this.txtRaio);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.MenuText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -45,10 +68,14 @@
             this.Text = "CALCULADORA DE ÁREA";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRaio;
 
     }
 }
