178a6344a59d271e1af92d5473c2f318210eb8da
Igor Enrico <igorefp@gmail.com>
2022-02-17 19:15:53 -0300

    update projetoCalculadoraArea

--- Projetos-C#/CalculadoraArea/projetoCalculadoraArea/Form1.cs
+++ Projetos-C#/CalculadoraArea/projetoCalculadoraArea/Form1.cs
@@ -16,5 +16,10 @@ namespace projetoCalculadoraArea
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
