c192b00ba9e70f6b38632b377479d4eefb727232
Igor Enrico <igorefp@gmail.com>
2022-02-17 19:20:25 -0300

    update projetoCalculadoraArea

--- Projetos-C#/CalculadoraArea/projetoCalculadoraArea/Form1.Designer.cs
+++ Projetos-C#/CalculadoraArea/projetoCalculadoraArea/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(380, 338);
+            this.ClientSize = new System.Drawing.Size(471, 378);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
