05cd03e19247c79a156873228e0d3b8ecf0ff3de
Igor Enrico <igorefp@gmail.com>
2022-02-17 19:18:22 -0300

    update projetoCalculadoraArea

--- Projetos-C#/CalculadoraArea/projetoCalculadoraArea/Form1.Designer.cs
+++ Projetos-C#/CalculadoraArea/projetoCalculadoraArea/Form1.Designer.cs
@@ -34,16 +34,19 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(359, 322);
+            this.ClientSize = new System.Drawing.Size(380, 338);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
+            this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Calculadora Área";
+            this.Text = "Calculadora de Área";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
     }
 }
 
