f176fa0cacfff666dee37192974f2a1075b24314
Igor Enrico <igorefp@gmail.com>
2022-02-17 19:36:44 -0300

    update projetoCalculadoraArea

--- Projetos-C#/CalculadoraArea/projetoCalculadoraArea/Form1.Designer.cs
+++ Projetos-C#/CalculadoraArea/projetoCalculadoraArea/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace projetoCalculadoraArea
 {
-    partial class Form1
+    partial class btnCalc
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -30,6 +30,7 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.txtRaio = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -45,24 +46,36 @@
             // txtRaio
             // 
             this.txtRaio.Location = new System.Drawing.Point(137, 59);
+            this.txtRaio.MaxLength = 10;
             this.txtRaio.Name = "txtRaio";
             this.txtRaio.Size = new System.Drawing.Size(201, 20);
             this.txtRaio.TabIndex = 1;
             this.txtRaio.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // Form1
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(137, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(201, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "CALCULAR";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnCalc
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSkyBlue;
             this.ClientSize = new System.Drawing.Size(471, 378);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.txtRaio);
             this.Controls.Add(this.label1);
             this.ForeColor = System.Drawing.SystemColors.MenuText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.Name = "Form1";
+            this.Name = "btnCalc";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CALCULADORA DE ÁREA";
@@ -76,6 +89,7 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtRaio;
+        private System.Windows.Forms.Button button1;
 
     }
 }
--- Projetos-C#/CalculadoraArea/projetoCalculadoraArea/Form1.cs
+++ Projetos-C#/CalculadoraArea/projetoCalculadoraArea/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace projetoCalculadoraArea
 {
-    public partial class Form1 : Form
+    public partial class btnCalc : Form
     {
-        public Form1()
+        public btnCalc()
         {
             InitializeComponent();
         }
