39e2d34467b2b4b50bdbb8fcfe8abae4f82604c8
Igor Enrico <igorefp@gmail.com>
2022-02-18 20:43:10 -0300

    update

---
+++ Projetos-HTML-CSS/jsBasico/script.js
@@ -0,0 +1,9 @@
+function calcular() {
+        var valor_1 = document.getElementById("input_valor_1").value;
+        var valor_2 = document.getElementById("input_valor_2").value;
+        var soma = valor_1 + valor_2;
+
+        var valor_calc = 2;
+        var valor_calc_2 = 2;
+        var result_calc = valor_calc + valor_calc_2;
+}
