0007df7750670966aa20be5e96aeb4c0a944c0ce
Igor Enrico <igorefp@gmail.com>
2022-02-18 22:19:58 -0300

    update

---
+++ Projetos-HTML-CSS/exJS/script.js
@@ -0,0 +1,27 @@
+var int_1 = 10
+var int_2 = 8
+var int_3 = 3
+var int_4 = 5
+
+var real_1 = 10.1
+var real_2 = 0.02
+var real_3 = 1065.87
+var real_4 = 26.5
+
+var txt_1 = "texto"
+var txt_2 = "hello world"
+var txt_3 = "30"
+var txt_4 = "40"
+
+var bool_1 = true
+var bool_2 = false
+
+var x = 0
+x += 5
+console.log(x)
+
+var conc = "2" + "5" + "3" + "6" + "8" + "4"
+
+var count = 50
+count += 100
+console.log(count)
