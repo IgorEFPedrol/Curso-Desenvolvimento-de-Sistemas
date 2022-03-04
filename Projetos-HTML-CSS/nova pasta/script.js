dcd4b1498b2a423557613a5e8f7c33b246dd85e8
Igor Enrico <igorefp@gmail.com>
2022-03-03 22:41:40 -0300

    update script.js

--- Projetos-HTML-CSS/nova pasta/script.js
+++ Projetos-HTML-CSS/nova pasta/script.js
@@ -1,1 +1,43 @@
 var num1 = 0.0;
+var num2 = 0.0;
+var result;
+
+
+
+var soma = document.getElementById("botaoSoma").addEventListener("click",
+    function somar() {
+
+        var campo1 = document.getElementById("campo1").value;
+        var campo2 = document.getElementById("campo2").value;
+        result = parseFloat(campo1) + parseFloat(campo2);
+        window.alert(result);
+    })
+
+var multi = document.getElementById("botaoMulti").addEventListener("click",
+    function multiplicar() {
+
+        var campo1 = document.getElementById("campo1").value;
+        var campo2 = document.getElementById("campo2").value;
+        result = parseFloat(campo1) * parseFloat(campo2);
+        window.alert(result);
+    })
+
+var div = document.getElementById("botaoDiv").addEventListener("click",
+    function multiplicar() {
+
+        var campo1 = document.getElementById("campo1").value;
+        var campo2 = document.getElementById("campo2").value;
+        result = parseFloat(campo1) / parseFloat(campo2);
+        window.alert(result);
+    }) 
+
+var sub = document.getElementById("botaoSub").addEventListener("click",
+    function subtracao() {
+
+        
+        var campo1 = document.getElementById("campo1").value;
+        var campo2 = document.getElementById("campo2").value;
+        result = parseFloat(campo1) - parseFloat(campo2);
+        window.alert(result); 
+
+    })
