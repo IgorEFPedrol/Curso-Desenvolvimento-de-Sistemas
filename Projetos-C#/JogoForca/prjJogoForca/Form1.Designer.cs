f8cc763c4bc2036a26041ba4fa62f7be3af1a445
Igor Enrico <igorefp@gmail.com>
2022-03-10 21:21:48 -0300

    update prjJogoForca

--- Projetos-C#/JogoForca/prjJogoForca/Form1.Designer.cs
+++ Projetos-C#/JogoForca/prjJogoForca/Form1.Designer.cs
@@ -28,9 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(702, 626);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "JOGO DA FORCA";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
