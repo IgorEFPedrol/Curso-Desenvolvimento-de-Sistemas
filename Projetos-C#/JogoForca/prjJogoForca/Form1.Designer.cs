64e9ec3cbf301eb54f86f5226e9011cf0b740f7a
Igor Enrico <igorefp@gmail.com>
2022-03-10 21:48:03 -0300

    update prjJogoForca

--- Projetos-C#/JogoForca/prjJogoForca/Form1.Designer.cs
+++ Projetos-C#/JogoForca/prjJogoForca/Form1.Designer.cs
@@ -28,24 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pnLetra = new System.Windows.Forms.Panel();
+            this.lbLetra = new System.Windows.Forms.Label();
+            this.txtLetra = new System.Windows.Forms.TextBox();
+            this.btnJogar = new System.Windows.Forms.Button();
+            this.pnPalavra = new System.Windows.Forms.Panel();
+            this.pnLetra.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pnLetra
+            // 
+            this.pnLetra.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnLetra.Controls.Add(this.btnJogar);
+            this.pnLetra.Controls.Add(this.txtLetra);
+            this.pnLetra.Controls.Add(this.lbLetra);
+            this.pnLetra.Location = new System.Drawing.Point(12, 12);
+            this.pnLetra.Name = "pnLetra";
+            this.pnLetra.Size = new System.Drawing.Size(201, 157);
+            this.pnLetra.TabIndex = 0;
+            // 
+            // lbLetra
+            // 
+            this.lbLetra.BackColor = System.Drawing.Color.Black;
+            this.lbLetra.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lbLetra.ForeColor = System.Drawing.Color.White;
+            this.lbLetra.Location = new System.Drawing.Point(0, 0);
+            this.lbLetra.Name = "lbLetra";
+            this.lbLetra.Size = new System.Drawing.Size(199, 36);
+            this.lbLetra.TabIndex = 0;
+            this.lbLetra.Text = "DIGITE LETRA";
+            this.lbLetra.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtLetra
+            // 
+            this.txtLetra.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLetra.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtLetra.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold);
+            this.txtLetra.Location = new System.Drawing.Point(16, 48);
+            this.txtLetra.MaxLength = 1;
+            this.txtLetra.Name = "txtLetra";
+            this.txtLetra.Size = new System.Drawing.Size(169, 32);
+            this.txtLetra.TabIndex = 1;
+            this.txtLetra.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnJogar
+            // 
+            this.btnJogar.BackColor = System.Drawing.Color.Silver;
+            this.btnJogar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnJogar.ForeColor = System.Drawing.Color.Black;
+            this.btnJogar.Location = new System.Drawing.Point(0, 90);
+            this.btnJogar.Name = "btnJogar";
+            this.btnJogar.Size = new System.Drawing.Size(199, 65);
+            this.btnJogar.TabIndex = 2;
+            this.btnJogar.Text = "JOGAR";
+            this.btnJogar.UseVisualStyleBackColor = false;
+            // 
+            // pnPalavra
+            // 
+            this.pnPalavra.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnPalavra.Location = new System.Drawing.Point(245, 13);
+            this.pnPalavra.Name = "pnPalavra";
+            this.pnPalavra.Size = new System.Drawing.Size(556, 155);
+            this.pnPalavra.TabIndex = 1;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(702, 626);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(813, 484);
+            this.Controls.Add(this.pnPalavra);
+            this.Controls.Add(this.pnLetra);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "JOGO DA FORCA";
+            this.pnLetra.ResumeLayout(false);
+            this.pnLetra.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnLetra;
+        private System.Windows.Forms.Label lbLetra;
+        private System.Windows.Forms.TextBox txtLetra;
+        private System.Windows.Forms.Button btnJogar;
+        private System.Windows.Forms.Panel pnPalavra;
     }
 }
 
