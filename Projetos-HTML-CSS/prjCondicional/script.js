6ee39aa14612b72b7bda6cf2da2eced6c315a7a4
Igor Enrico <igorefp@gmail.com>
2022-03-11 19:58:53 -0300

    Update script.js

--- Projetos-HTML-CSS/prjCondicional/script.js
+++ Projetos-HTML-CSS/prjCondicional/script.js
@@ -2,8 +2,9 @@ var botao = document.getElementById("botao_OK").addEventListener("click",
     function pegarValores() {
         var campo1 = document.getElementById("campo1").value;
         var campo2 = document.getElementById("campo2").value;
+        verificarCampos(campo1, campo2);
     })
 
 function verificarCampos(campoNumero1, campoNumero2) {
-    
+     
 }
