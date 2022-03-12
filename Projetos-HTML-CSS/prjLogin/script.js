4bedec851f957f06e37244a7f372ec7a78bc4aa3
Igor Enrico <igorefp@gmail.com>
2022-03-11 22:08:17 -0300

    update prjLogin

--- Projetos-HTML-CSS/prjLogin/script.js
+++ Projetos-HTML-CSS/prjLogin/script.js
@@ -1,1 +1,20 @@
-var email = document.getElementById("")
+const loginEmail = "igor@gmail.com";
+const loginSenha = 123456;
+
+var botao = document.getElementById("button_login").addEventListener("click", 
+    function pegarValores() {
+        var email = document.getElementById("email_user").value;
+        var senha = document.getElementById("senha_user").value;
+        verificarCampos(email, senha)
+    })
+
+function verificarCampos(campo1, campo2) {
+    if (campo1 == "" || campo2 == "") {
+        window.alert("Você precisa preencher os campos!!!")
+    } 
+    else if (campo1 == loginEmail && campo2 == loginSenha) {
+        window.alert("Login Realizado")
+    } else {
+        window.alert("Login Inválido!!!")
+    }
+}
