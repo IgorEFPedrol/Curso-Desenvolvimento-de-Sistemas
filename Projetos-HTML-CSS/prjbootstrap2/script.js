5322a6bfdcd3e71e3c87c8203fe206ff5696dfe0
Igor Enrico <igorefp@gmail.com>
2022-03-22 20:47:53 -0300

    update

---
+++ Projetos-HTML-CSS/prjbootstrap2/script.js
@@ -0,0 +1,7 @@
+
+var botao = document.getElementById("btnCalcular").addEventListener("click",
+    function mostrarResult() {
+        var celsius = 0.0;
+        var celsius = document.getElementById("valorConvert");
+        window.alert(celsius + 273)
+})
