11dc61b1f710b176a9061234ff548ff3182d156d
Igor Enrico <igorefp@gmail.com>
2022-03-29 19:41:12 -0300

    Create index.php

---
+++ Projetos-HTML-CSS/prjbootstrap3_/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+    <title></title>
+</head>
+<body>
+    <div class="container"> 
+        <div class="col">
+            <label for="input_email">Login</label>
+            <input type="email" id="input_email" placeholder="Insira o seu E-mail">
+        </div>
+        <div class="col">
+            <label for="input_senha">Senha</label>
+            <input type="password" id="input_senha">
+        </div>
+        <div class="col">
+            <button type="submit" id="btn_enviar">Enviar</button>
+        </div>
+    </div>
+</body>
+</html>
