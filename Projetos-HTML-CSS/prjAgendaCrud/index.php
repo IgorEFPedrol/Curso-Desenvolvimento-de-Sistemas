5a91051ad19550b464b6c2554c2e0c6621261284
Igor Enrico <igorefp@gmail.com>
2022-04-05 15:16:11 -0300

    Create index.php

---
+++ Projetos-HTML-CSS/prjAgendaCrud/index.php
@@ -0,0 +1,54 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Agenda Pessoal</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-3 bg-info h-1000">
+            
+            </div>
+            <div class="col-9 bg-success">
+            <h1>Agenda Pessoal</h1>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>id_compromisso</th>
+                        <th>data</th>
+                        <th>descricao</th>
+                        <th>tipo</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>$idCliente</td>
+                        <td>$data</td>
+                        <td>$descricao</td>
+                        <td>$tipo</td>
+                    </tr>
+                </tbody>
+
+            </table>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+</body>
+</html>
+
+
+
