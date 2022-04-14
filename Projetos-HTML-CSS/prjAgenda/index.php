2833609cceff6f4b0fa8705e2f716acab2b34b99
Igor Enrico <igorefp@gmail.com>
2022-04-13 22:21:22 -0300

    update prjAgenda

--- Projetos-HTML-CSS/prjAgenda/index.php
+++ Projetos-HTML-CSS/prjAgenda/index.php
@@ -5,50 +5,54 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
   <title>Login</title>
   <link rel="icon" type="image/x-icon" href="assets/icone.svg">
 
 </head>
 
-<body>
-  <div class="container">
-    <div>
-      <p class="text-center fs-1 mt-5 fw-bold">Login</p>
-    </div>
-    <div>
-      <p class="text-center col-12 fs-6 fw-normal">Preencha os campos abaixo com seus dados cadastrados no site</p>
-    </div>
-
-    <form action="login.php" method="post">
-      <div class="row d-flex col-6 d-grid mx-auto mt-5">
-      <?php if (isset($_GET['erro'])) { ?>
-     		<p class="erro bg-danger"><?php echo $_GET['erro']; ?></p>
-        <?php } ?>
-        <label for="Email" class="fw-bold">E-mail</label>
-        <input type="text" class="form-control my-1" id="Email" name="email" placeholder="@dominio.com" />
-
-        <label for="Senha" class="fw-bold mt-3">Senha</label>
-        <input type="password" class="form-control my-1" id="Senha" name="senha" placeholder="Senha" />
+<body style="background-image: url(assets/iMac\ -\ 3.png);">
+  <div class="container-fluid">
+    <div class="row justify-content-end d-flex" style="margin-right: 200px; margin-top: 192px">
+      <div class="col-6">
+        <div class="card col-10 pb-4 rounded">
+          <div class="card-body">
+      <div>
+        <p class="text-center fs-1 fw-bold mt-4" style="color: #944ACE;">Login</p>
+          <p class="text-center">Preencha os campos abaixo com seus dados cadastrados no site</p>
+      
+            <form action="login.php" method="post">
+              <div class="row d-flex col-6 d-grid mx-auto mt-5">
+                <?php if (isset($_GET['erro'])) { ?>
+                  <p class="erro fw-bold bg-transparent" style="color: #bb2124;"><?php echo $_GET['erro']; ?></p>
+               <?php } ?>
+              <label for="Email" class="mt-2 fw-bold">E-mail</label>
+                <input type="text" class="form-control mt-2" id="Email" name="email" placeholder="@dominio.com" />
+              <label for="Senha" class="mt-3 fw-bold">Senha</label>
+                <input type="password" class="form-control mt-2" id="Senha" name="senha" placeholder="Senha" />
+              </div>
+              <div class="row">
+              <p class="text-center mt-5 fs-6">
+              Esqueci minha senha!
+              <a href="" class="link-primary fw-bold" style="color: #944ACE;"> Altere ela aqui</a>
+            </p>
+            <p class="text-center fs-6">
+              Não tem conta? <a href="" class="link-primary fw-bold" style="color: #944ACE;"> Crie uma já</a>
+            </p>
+            <div class="row col-6  d-grid mx-auto">
+              <button type="submit" class="btn btn-primary border-0 mt-5 col-12" id="botaoEntrar" style="background-color: #944ACE;">Entrar</button>
+            </div>
+          </form>
+          </div>
+        </div>
+        </div>
+        </div>
       </div>
-      <div class="row">
-        <p class="text-center mt-5">Esqueci minha senha! <a href="#" class="link-primary fw-bold">Altere aqui</a></p>
-        <p class="text-center mb-4 ">Não tem uma conta? <a href="#" class="link-primary fw-bold">Criar uma já</a></p>
-      </div>
-
-      <div class="row col-6 mt-3 d-grid mx-auto">
-        <button type="submit" class="btn btn-primary" id="botaoEntrar" style="background-color: black;">Entrar</button>
-      </div>
-    </form>
-
   </div>
 
 
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
-    integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
-    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 </body>
 
 </html>
