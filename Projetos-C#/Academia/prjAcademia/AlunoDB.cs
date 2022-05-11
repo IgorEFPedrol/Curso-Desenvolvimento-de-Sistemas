ef3f4d0bd5dd4a8f0ca79b95d44b11f9a71c26ca
Igor Enrico <igorefp@gmail.com>
2022-05-11 19:36:14 -0300

    Update AlunoDB.cs

--- Projetos-C#/Academia/prjAcademia/AlunoDB.cs
+++ Projetos-C#/Academia/prjAcademia/AlunoDB.cs
@@ -12,11 +12,31 @@ namespace prjAcademia
         public void Inserir(Aluno reg)
         {
             ServidorSQL academia = new ServidorSQL();
+
             using (var banco = new SQLiteCommand(academia.Open()))
             {
-
+                string sql = string.Format("INSERT INTO ALUNO(ID, NOME, ALTURA)" +
+                    "VALUES ({0}, {1}, {2}, {3}, {4})",
+                    reg.ID, reg.Nome, reg.Idade, reg.Peso,
+                    reg.Altura.ToString().Replace(',', '.') + ")");
+                banco.CommandText = sql;
+                banco.ExecuteNonQuery();
             }
         }
+        public void Editar (Aluno reg)
+        {
+             ServidorSQL academia = new ServidorSQL();
 
+             using (var banco = new SQLiteCommand(academia.Open()))
+             {
+                 string sql = String.Format("UPDATE ALUNO SET NOME = '{0}'" +
+                     ", IDADE = {1}, PESO = {2}, ALTURA = {3}) " +
+                     "WHERE ID = {4}", reg.Nome, reg.Idade, reg.Peso,
+                     reg.Altura.ToString().Replace(',', '.'), reg.ID);
+                 banco.CommandText = sql;
+                 banco.ExecuteNonQuery();
+             }
+        }
+            
     }
 }
