f424b3d946253e3f3123aa963d9a775b0a4487a8
Igor Enrico <igorefp@gmail.com>
2022-06-01 19:28:05 -0300

    CREATE prjCalculadora

---
+++ Projetos-C#/Calculadora/prjCalculadora/Calculadora.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjCalculadora
+{
+    class Calculadora
+    {
+        public string Visor { get; set; }
+        public double Aux1 { get; set; }
+        public double Aux2 { get; set; }
+        public string Op { get; set; }
+
+        public Calculadora()
+        {
+            Visor = "0";
+            Aux1 = 0f;
+            Aux2 = 0f;
+            Op = "";
+        }
+
+        public void SetVisor(string digito)
+        {
+            if (Visor.Equals("0"))
+                Visor = digito;
+            else Visor += digito;
+        }
+    }        
+}            
