d8bb1e252b2304b751d05d0da406ef1414d63174
Igor Enrico <igorefp@gmail.com>
2022-08-17 21:45:50 -0300

    Create API-Matematica

---
+++ Projetos-QTS/solucaoAPI/API_Matematica/bibliotecaMatematica.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_Matematica
+{
+    public class BibliotecaMatematica
+    {
+        public static bool Par(int num)
+        {
+            return num % 2 == 0 ? true : false;
+        }
+        public static bool Primo(int num)
+        {
+            if (Par(num) && num != 2) return false;
+            int cont = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0) cont++;
+            }
+            return cont == 2 ? true : false;
+        }
+    }
+}
---
+++ Projetos-QTS/solucaoAPI/Teste_console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using API_Matematica;
+
+namespace Teste_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("{0}\t Par: {1}\t Primo: {2}", i,
+                    BibliotecaMatematica.Par(i),
+                    BibliotecaMatematica.Primo(i));
+            }
+            Console.ReadKey();
+        }
+    }
+}
