0166e26b45136e277cb88493534e33844f5ea514
Igor Enrico <igorefp@gmail.com>
2022-08-24 22:27:29 -0300

    Update Program.cs

--- Projetos-QTS/EXTENSO/TESTE_EXTENSO/Program.cs
+++ Projetos-QTS/EXTENSO/TESTE_EXTENSO/Program.cs
@@ -12,11 +12,16 @@ namespace TESTE_EXTENSO
         static void Main(string[] args)
         {
             Console.Clear();
+            DateTime inicio = DateTime.Now;
             for (int i = 0; i < 10000; i++)
             {
                 Console.WriteLine("{0} = {1}",
                     i, APIExtenso.Imprimir(i));
             }
+            DateTime fim = DateTime.Now;
+            TimeSpan tempo = new TimeSpan();
+            tempo = fim - inicio;
+            Console.WriteLine("Tempo: {0}", tempo.TotalSeconds);
             Console.ReadKey();
         }
     }
