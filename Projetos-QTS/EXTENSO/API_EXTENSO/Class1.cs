993a2aef50306856d827a364d2725c96bcb76f12
Igor Enrico <igorefp@gmail.com>
2022-08-24 21:27:48 -0300

    CREATE API_EXTENSO

---
+++ Projetos-QTS/EXTENSO/API_EXTENSO/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_EXTENSO
+{
+    public class APIExtenso
+    {
+        public static string Imprimir(int n)
+        {
+            if (n < 10) return Unidades(n);
+            else return "";
+        }
+
+        public static string Unidades(int n)
+        {
+            string[] u = new string[] {
+                "zero", "um", "dois", "três",
+                "quatro", "cinco", "seis", "oito", "nove"
+            };
+            return u[n];
+        }
+    }
+}
