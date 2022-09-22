3add8a060cd6903ced9b558cc3e8c1b7d0761d03
Igor Enrico <igorefp@gmail.com>
2022-09-21 21:25:27 -0300

    create GoogleAPI

---
+++ Projetos-QTS/BUSCA_LIVRO/API_LIVRO/GoogleAPI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace API_LIVRO
+{
+    public class GoogleAPI
+    {
+        public async Task<string> Conectar(string url)
+        {
+            using (var cliente = new HttpClient())
+            {
+
+            }
+        }
+    }
+}
