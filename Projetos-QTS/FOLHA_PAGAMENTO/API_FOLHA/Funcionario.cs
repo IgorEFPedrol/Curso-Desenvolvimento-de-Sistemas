017e3a75dcf16bd345809d7f0dba5d6b19f3c8b9
Igor Enrico <igorefp@gmail.com>
2022-09-28 21:32:16 -0300

    Create API_FOLHA

---
+++ Projetos-QTS/FOLHA_PAGAMENTO/API_FOLHA/Funcionario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_FOLHA
+{
+    class Funcionario:ICalculoSalario
+    {
+        public int Cod { get; set; }
+        public string Nome { get; set; }
+        public double Horas { get; set; }
+
+        public Funcionario(int Cod, string Nome, double Horas)
+        {
+            this.Cod = Cod;
+            this.Nome = Nome;
+            this.Horas = Horas;
+        }
+        public Funcionario():this(0, "", 0f)
+        {
+
+        }
+        public double Calcular(double SalarioHora)
+        {
+            double sb = Horas * SalarioHora;
+            return sb;
+        }
+        public double Desconto(double Salario)
+        {
+            double valor = Salario * 0.02;
+            return valor;
+        }
+    }
+}
---
+++ Projetos-QTS/FOLHA_PAGAMENTO/TESTE_FOLHA/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TESTE_FOLHA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
