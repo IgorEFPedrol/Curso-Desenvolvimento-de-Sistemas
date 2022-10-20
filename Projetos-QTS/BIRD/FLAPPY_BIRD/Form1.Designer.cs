156a6753564219eb784f84a258c8a4e8c453a0b7
Igor Enrico <igorefp@gmail.com>
2022-10-19 22:02:22 -0300

    create flappy bird

---
+++ Projetos-QTS/BIRD/FLAPPY_BIRD/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace FLAPPY_BIRD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">verdade se for necessário descartar os recursos gerenciados; caso contrário, falso.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte do Designer - não modifique
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bird = new System.Windows.Forms.PictureBox();
+            this.jogo = new System.Windows.Forms.Timer(this.components);
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.tuboInferior = new System.Windows.Forms.PictureBox();
+            this.tuboSuperior = new System.Windows.Forms.PictureBox();
+            this.lbPlacar = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tuboInferior)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tuboSuperior)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bird
+            // 
+            this.bird.Image = ((System.Drawing.Image)(resources.GetObject("bird.Image")));
+            this.bird.Location = new System.Drawing.Point(109, 209);
+            this.bird.Name = "bird";
+            this.bird.Size = new System.Drawing.Size(50, 50);
+            this.bird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bird.TabIndex = 0;
+            this.bird.TabStop = false;
+            // 
+            // jogo
+            // 
+            this.jogo.Enabled = true;
+            this.jogo.Tick += new System.EventHandler(this.jogo_Tick);
+            // 
+            // ground
+            // 
+            this.ground.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.ground.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ground.BackgroundImage")));
+            this.ground.Location = new System.Drawing.Point(-1, 399);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(888, 96);
+            this.ground.TabIndex = 1;
+            this.ground.TabStop = false;
+            // 
+            // tuboInferior
+            // 
+            this.tuboInferior.Image = ((System.Drawing.Image)(resources.GetObject("tuboInferior.Image")));
+            this.tuboInferior.Location = new System.Drawing.Point(616, 218);
+            this.tuboInferior.Name = "tuboInferior";
+            this.tuboInferior.Size = new System.Drawing.Size(75, 183);
+            this.tuboInferior.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tuboInferior.TabIndex = 2;
+            this.tuboInferior.TabStop = false;
+            // 
+            // tuboSuperior
+            // 
+            this.tuboSuperior.Image = ((System.Drawing.Image)(resources.GetObject("tuboSuperior.Image")));
+            this.tuboSuperior.Location = new System.Drawing.Point(420, -2);
+            this.tuboSuperior.Name = "tuboSuperior";
+            this.tuboSuperior.Size = new System.Drawing.Size(75, 156);
+            this.tuboSuperior.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tuboSuperior.TabIndex = 3;
+            this.tuboSuperior.TabStop = false;
+            // 
+            // lbPlacar
+            // 
+            this.lbPlacar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(216)))), ((int)(((byte)(149)))));
+            this.lbPlacar.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPlacar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lbPlacar.Location = new System.Drawing.Point(665, 441);
+            this.lbPlacar.Name = "lbPlacar";
+            this.lbPlacar.Size = new System.Drawing.Size(206, 40);
+            this.lbPlacar.TabIndex = 4;
+            this.lbPlacar.Text = "placar = 0";
+            this.lbPlacar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(883, 490);
+            this.Controls.Add(this.lbPlacar);
+            this.Controls.Add(this.tuboSuperior);
+            this.Controls.Add(this.tuboInferior);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.bird);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tuboInferior)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tuboSuperior)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox bird;
+        private System.Windows.Forms.Timer jogo;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.PictureBox tuboInferior;
+        private System.Windows.Forms.PictureBox tuboSuperior;
+        private System.Windows.Forms.Label lbPlacar;
+    }
+}
+
---
+++ Projetos-QTS/BIRD/FLAPPY_BIRD/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FLAPPY_BIRD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int gravidade = 15;
+        int speed = 25  ;
+        int placar = 0;
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravidade = -20;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravidade = 15;
+            }
+        }
+
+        private void jogo_Tick(object sender, EventArgs e)
+        {
+            bird.Top += gravidade;
+            tuboInferior.Left -= speed;
+            tuboSuperior.Left -= speed;
+            if (tuboInferior.Left < 0 - tuboInferior.Width * 2)
+            {
+                tuboInferior.Left = this.Width + tuboInferior.Width * 2;
+                placar++;
+            }
+            if (tuboSuperior.Left < 0 - tuboSuperior.Width * 2)
+            {
+                tuboSuperior.Left = this.Width + tuboSuperior.Width * 2;
+                placar++;
+            }
+            lbPlacar.Text = String.Format("PLACAR: {0}", placar.ToString().PadLeft(4, '0'));
+        }
+
+        
+    }
+}
