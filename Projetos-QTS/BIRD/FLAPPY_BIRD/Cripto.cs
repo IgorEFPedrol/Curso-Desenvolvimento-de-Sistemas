fb3ecee0bdf009c8716290783e070d2ec5eadc67
Igor Enrico <igorefp@gmail.com>
2022-11-16 22:15:17 -0300

    Update Cripto.cs

--- Projetos-QTS/BIRD/FLAPPY_BIRD/Cripto.cs
+++ Projetos-QTS/BIRD/FLAPPY_BIRD/Cripto.cs
@@ -17,6 +17,23 @@ namespace FLAPPY_BIRD
             UTF8Encoding UTF8 = new UTF8Encoding();
             MD5CryptoServiceProvider Provedor = new MD5CryptoServiceProvider();
             byte[] TDESKey = Provedor.ComputeHash(UTF8.GetBytes(chave));
+            TripleDESCryptoServiceProvider TDESAlgoritmo = new TripleDESCryptoServiceProvider();
+            TDESAlgoritmo.Mode = CipherMode.ECB;
+            TDESAlgoritmo.Key = TDESKey;
+            TDESAlgoritmo.Padding = PaddingMode.PKCS7;
+            if (texto.Equals("")) return "";
+            byte[] dados = UTF8.GetBytes(texto);
+            try
+            {
+                ICryptoTransform Enc = TDESAlgoritmo.CreateEncryptor();
+                Results = Enc.TransformFinalBlock(dados, 0, dados.Length);
+            }
+            finally
+            {
+                TDESAlgoritmo.Clear();
+                Provedor.Clear();
+            }
+            return Convert.ToBase64String(Results);
         } 
     }
 }
