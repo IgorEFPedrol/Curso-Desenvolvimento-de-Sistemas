1a06ed667b6b038ec03ae665140bed63d3427128
Igor Enrico <igorefp@gmail.com>
2022-11-16 22:01:58 -0300

    update

--- Projetos-QTS/BIRD/FLAPPY_BIRD/Cripto.cs
+++ Projetos-QTS/BIRD/FLAPPY_BIRD/Cripto.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +11,12 @@ namespace FLAPPY_BIRD
     {
         const string chave = "1593574862";
 
+        public static string Codificar(string texto)
+        {
+            byte[] Results;
+            UTF8Encoding UTF8 = new UTF8Encoding();
+            MD5CryptoServiceProvider Provedor = new MD5CryptoServiceProvider();
+            byte[] TDESKey = Provedor.ComputeHash(UTF8.GetBytes(chave));
+        } 
     }
 }
